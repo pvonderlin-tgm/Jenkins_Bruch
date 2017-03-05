95cc5573054b4bf1182f3d329fcc4fdaddd649c0
Philip Vonderlind <pvonderlin@student.tgm.ac.at>
2017-03-05 12:41:18 +0000

    Inserted 5 new Testcases

--- Testall.py
+++ Testall.py
@@ -244,7 +244,7 @@ class TestMultiplikation(unittest.TestCase):
         assert(float(self.b) == 6)
 
     def testmal2(self):
-        self.b = self.b * self.b2
+        self.b *= self.b2
         assert(float(self.b) == 2.25)
 
     def testmal3(self):
@@ -353,6 +353,40 @@ class TestIteration(unittest.TestCase):
     @staticmethod
     def tryIndex(obj, index):
         return obj[index]
+
+
+class TestExtension(unittest.TestCase):
+
+    def setUp(self):
+        self.b = Bruch(3, 2)
+        self.b2 = Bruch(self.b)
+        self.b3 = Bruch(4, 2)
+        pass
+
+    def tearDown(self):
+        del self.b, self.b2, self.b3
+        pass
+
+    def testDoubleDivision(self):
+        self.b = self.b / self.b2 / self.b3
+        assert (float(self.b) == 0.5)
+
+    def testDoubleMultiplikation(self):
+        self.b = self.b * self.b2 * self.b3
+        assert (float(self.b) == 4.5)
+
+    def testTupleMult(self):
+        z, n = Bruch(3, 4) * Bruch(1, 2)
+        assert (Bruch(z, n) == Bruch(3, 8))
+
+    def testTupleDiv(self):
+        z, n = Bruch(8, 7) * Bruch(2, 5)
+        assert (Bruch(z, n) == Bruch(16, 35))
+
+    def testComplexSub(self):
+        z = complex(self.b3) - complex(self.b)
+        assert (z == 0.5)
+
 
 if __name__ == "__main__":
     unittest.main()
---
+++ Unit_Extension.py
@@ -0,0 +1,35 @@
+import unittest
+from bruch.Bruch import *
+
+
+class TestExtension(unittest.TestCase):
+
+    def setUp(self):
+        self.b = Bruch(3, 2)
+        self.b2 = Bruch(self.b)
+        self.b3 = Bruch(4, 2)
+        pass
+
+    def tearDown(self):
+        del self.b, self.b2, self.b3
+        pass
+
+    def testDoubleDivision(self):
+        self.b = self.b / self.b2 / self.b3
+        assert (float(self.b) == 0.5)
+
+    def testDoubleMultiplikation(self):
+        self.b = self.b * self.b2 * self.b3
+        assert (float(self.b) == 4.5)
+
+    def testTupleMult(self):
+        z, n = Bruch(3, 4) * Bruch(1, 2)
+        assert (Bruch(z, n) == Bruch(3, 8))
+
+    def testTupleDiv(self):
+        z, n = Bruch(8, 7) * Bruch(2, 5)
+        assert (Bruch(z, n) == Bruch(16, 35))
+
+    def testTupleSubAndAdd(self):
+        z, n = Bruch(1, 3) + self.b - Bruch(4/8)
+        assert (Bruch(z, n) == Bruch(4/3))
